566dc9be48354b6664535baf12c84bb3edcb1e31
Hamza Nasir <hk435981@gmail.com>
2024-11-05 21:00:26 +0500

    responsive Done

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
 
   return (
     <div
-        className="w-full ml-[300px] h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+        className="w-full sm:ml-[300px] h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
         style={{
             backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
         }}
